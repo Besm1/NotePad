85bb0b7cd9bb92c6ec821c7ec90d0af9b40cafb6
b.smirnov <besm1@yandex.ru>
2024-09-16 21:55:35 +0300

    Первое сохранение

--- main.py
+++ main.py
@@ -1,60 +1,128 @@
 from cProfile import label
+from ctypes import windll
 from tkinter import *
+from tkinter.messagebox import showinfo, askyesno
+from tkinter.scrolledtext import ScrolledText
+
 import keyboard
 
 class Notepad():
 
 
     def __init__(self):
-        self.window = Tk()
-        self.window.geometry('260x220')
-        self.window.resizable(True, True)
-        self.window.minsize(width=260, height=220)
+        self.root = Tk()
+        self.root.geometry('260x220')
+        self.root.resizable(True, True)
+        self.root.minsize(width=260, height=220)
+        self.root.title('BS_Notepad')
 
-        self.window.option_add('*tearOff', FALSE)   # глобальная установка параметра для меню
+        self.root.option_add('*tearOff', FALSE)   # глобальная установка параметра для меню
 
         self.file_menu = Menu()
-        self.file_menu.add_command(label='Создать')
-        self.file_menu.add_command(label='Открыть...')
-        self.file_menu.add_command(label='Сохранить')
-        self.file_menu.add_command(label='Сохранить как...')
-        self.file_menu.add_separator()
-        self.file_menu.add_command(label='Выход', command=self.cmd_edit)
+        self.set_cascade(self.file_menu, 'Создать', 'Открыть...', 'Сохранить', "Сохранить как..."
+                         , '__sep__', ('Выход', self.cmd_exit))
 
         self.edit_menu = Menu()
-        self.edit_menu.add_command(label='Отменить    CTRL/Z')
-        self.edit_menu.add_command(label='Вырезать    CTRL/X')
-        self.edit_menu.add_command(label='Копировать  CTRL/C')
-        self.edit_menu.add_command(label='Вставить    CTRL/V')
-        self.edit_menu.add_command(label='Удалить')
-        self.edit_menu.add_separator()
-        self.edit_menu.add_command(label='Заменить...  CTRL/H')
-        self.edit_menu.add_command(label='Перейти...   CTRL/G')
-        self.edit_menu.add_separator()
-        self.edit_menu.add_command(label='Найти...     CTRL/F')
-        self.edit_menu.add_command(label='Найти далее  F3')
+        self.set_cascade(self.edit_menu, 'Отменить    CTRL/Z'
+                         , 'Вырезать    CTRL/X'
+                         , 'Копировать  CTRL/C'
+                         , 'Вставить    CTRL/V'
+                         , 'Удалить'
+                         , '___sep__'
+                         , 'Заменить...  CTRL/H'
+                         , 'Перейти...   CTRL/G'
+                         , '__sep__'
+                         , 'Найти...     CTRL/F'
+                         , 'Найти далее  F3'
+                         )
 
         self.help_menu = Menu()
-        self.help_menu.add_command(label='Помощь пользователю...')
-        self.help_menu.add_command(label='О программе')
+        self.set_cascade(self.help_menu, ('Помощь пользователю', self.show_help)
+                         , ('О программе', self.show_about))
 
         self.main_menu = Menu()
-        self.main_menu.add_cascade(label="Файл", menu=self.file_menu)
-        self.main_menu.add_cascade(label="Правка", menu=self.edit_menu)
-        self.main_menu.add_cascade(label="Справка", menu=self.help_menu)
+        self.set_cascade(self.main_menu, ('__cascade__', "Файл", self.file_menu)
+                         , ('__cascade__', "Правка", self.edit_menu)
+                         , ('__cascade__', "Справка", self.help_menu)
+                         )
 
-        self.window.config(menu=self.main_menu)
+        self.root.config(menu=self.main_menu)
 
-        self.window.bind('<Destroy>', self.w_destroy)
+        editor = ScrolledText(master=self.root)
+        editor.pack(fill=BOTH, expand=1)
 
-        self.window.mainloop()
+        # self.root.protocol("WM_DELETE_WINDOW", lambda: self.on_exit)
+
+        self.root.mainloop()
 
 
-    def w_destroy(self, event):
-        print(f'Размеры окна: {(self.window.winfo_width(), self.window.winfo_height())}')
+    def set_cascade(self, menu: Menu, *commands):
+        for c_ in commands:
+            if isinstance(c_, tuple) and len(c_) == 3:
+                spec, name, option = c_
+            elif isinstance(c_, tuple) and len(c_) == 2:
+                spec, name = c_
+                option = None
+            else:
+                spec, name, option = c_, None, None
+            if spec == '__sep__':
+                menu.add_separator()
+            elif spec == '__cascade__':
+                menu.add_cascade(label=name, menu=option)
+            elif isinstance(spec, str):
+                menu.add_command(label=spec, command=name)
+            else:
+                pass
 
-    def cmd_edit(self):
-        self.window.destroy()
+
+    def show_about(self):
+        showinfo('О программе', 'Программа NotePad\nАвтор - Смирнов Б.Е.\nВерсия 1.0, сборка 16.09.2024')
+
+    def cmd_exit(self):
+        if not askyesno(title='Внимание!', message='Вы сохранили текст?'):
+            showinfo(title='Ох, как печально!!', message='Шеф, всё пропало!!!')
+        self.root.destroy()
+
+    # def on_exit(self, wnd):
+
+
+
+    def show_help(self):
+        w_help = Window(geometry='300x250')
+        help = Label(master=w_help, text='''
+Программа BS_Notepad является аналогом
+программы NotePad.
+
+В основном окне есть меню, при помощи
+которого можно открывать/сохранять файлы,
+редактировать текст и получать помощь.
+ 
+Программа поставляется в виде "как есть".
+Вы её используете на свой риск, автор не
+несёт никакой ответственности за причинённый
+Вам вред от её использования.
+ 
+Если она Вам не нравится, то это Ваше личное
+дело, и никаких претензий автор не принимает. 
+        ''')
+        help.pack(fill=BOTH, expand=1)
+
+
+
+
+class Window(Tk):
+
+    windows_count = 0
+
+    def __init__(self, title='Window', geometry='800x600', resizable = (False, False)):
+        super().__init__()
+
+        self.windows_count += 1
+
+        self.title = title + (str(self.windows_count) if title == 'Window_' else '')
+        self.geometry(geometry)
+        self.resizable(*resizable)
+
 
 if __name__ == '__main__':
     np = Notepad()
